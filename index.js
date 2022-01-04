a88747ac01e989304808e47d71a9188292452e66
Jordi Roca <jordirocasoler94@gmail.com>
2022-01-04 11:30:03 +0100

    stripe

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart.js");
 const orderRoute = require("./routes/order");
-
+const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -16,13 +17,14 @@ mongoose
   .then(() => console.log("DB conected Successfull"))
   .catch((err) => console.log(err, "DB not conected"));
 
+app.use(cors());
 app.use(express.json());
-app.use("/api/auth",authRoute);
-app.use("/api/users",userRoute);
-app.use("/api/products",productRoute);
-app.use("/api/carts",cartRoute);
-app.use("/api/orders",orderRoute);
-
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running");
